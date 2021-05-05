ffd8d9d878e651a376b41e0e50ebba8d68e67f00
maxjanney <majanney@me.com>
2021-05-05 10:17:44 -0400

    poly types

--- poly.v
+++ poly.v
@@ -4,9 +4,24 @@ Inductive list (X : Type) : Type :=
   | nil
   | cons (x : X) (l : list X).
 
-Fixpoint repeat (X : Type) (x : X) (count : nat) : list X :=
+Arguments nil {X}.
+Arguments cons {X} _ _.
+
+Fixpoint repeat {X : Type} (x : X) (count : nat) : list X :=
   match count with
-  | 0 => nil X
-  | S count' => cons X x (repeat X x count')
+  | 0 => nil
+  | S k => cons x (repeat x k)
+  end.
+
+Fixpoint app {X : Type} (l1 l2 : list X) : list X :=
+  match l1 with
+  | nil => l2
+  | cons h t => cons h (app t l2)
+  end.
+
+Fixpoint rev {X : Type} (l : list X) : list X :=
+  match l with
+  | nil => nil
+  | cons h t => app (rev t) (cons h nil)
   end.
 
